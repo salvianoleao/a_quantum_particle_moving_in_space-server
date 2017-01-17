29107a371992f091c273c55c7a109bd39f111af9
Xiang Gao <qasdfgtyuiop@gmail.com>
2017-01-16 23:08:12 -0500

    fix data dependency

--- cgi-bin/pib/part3-el.py
+++ cgi-bin/pib/part3-el.py
@@ -27,6 +27,29 @@ else:
 
 	val = np.sqrt(2.0*9.10938356e-31*1.60217662e-19)*1e-10/(2.0*1.05457180013e-34) # equal to sqrt(2m*1eV)*1A/(2*hbar)
 
+	# Here we loop from E = 0 to E = Vo seeking roots
+	for E in np.linspace(0.0, Vo, 20000):
+	    f_even_now = f_even(E)
+	    # If the difference is zero or if it changes sign then we might have passed through a root
+	    if f_even_now == 0.0 or f_even_now/f_even_old < 0.0:
+	        # If the old values of f are not close to zero, this means we didn't pass through a root but
+	        # through a discontinuity point
+	        if (abs(f_even_now)<1.0 and abs(f_even_old)<1.0):
+	            E_vals[n-1] = (E+E_old)/2.0
+	            n += 1
+	    f_odd_now = f_odd(E)
+	    # If the difference is zero or if it changes sign then we might have passed through a root
+	    if f_odd_now == 0.0 or f_odd_now/f_odd_old < 0.0:
+	        # If the old values of f are not close to zero, this means we didn't pass through a root but
+	        # through a discontinuity point
+	        if (abs(f_odd_now)<1.0 and abs(f_odd_old)<1.0):
+	            E_vals[n-1] = (E+E_old)/2.0
+	            n += 1
+	    E_old = E
+	    f_even_old = f_even_now
+	    f_odd_old = f_odd_now
+	nstates = n-1
+
 	# Generating the energy diagram
 	fig, ax = plt.subplots(figsize=(8,12))
 	ax.spines['right'].set_color('none')
